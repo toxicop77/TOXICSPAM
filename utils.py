b873838dda79e9b8009c9c822ae53bca181bdc80
toxicop77 <107669917+toxicop77@users.noreply.github.com>
2023-05-14 12:01:26 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,14 +6,14 @@ from config import SUDO_USERS
 
 
 def load_plugins(plugin_name):
-    path = Path(f"AltronX/modules/{plugin_name}.py")
-    name = "AltronX.modules.{}".format(plugin_name)
+    path = Path(f"TOXICX/modules/{plugin_name}.py")
+    name = "TOXICX.modules.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["AltronX.modules." + plugin_name] = load
-    print("Altron has Imported " + plugin_name)
+    sys.modules["TOXICX.modules." + plugin_name] = load
+    print("TOXIC has Imported " + plugin_name)
 
 async def edit_or_reply(event, text):
     if event.sender_id in SUDO_USERS:
