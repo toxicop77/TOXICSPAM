719ebdc153ce443b1e06a761c9ba225b1517bf49
toxicop77 <107669917+toxicop77@users.noreply.github.com>
2023-05-14 07:36:24 +0530

    Update bot.py

--- TOXICX/modules/bot.py
+++ TOXICX/modules/bot.py
@@ -1,1 +1,133 @@
+import os
+import sys
+import heroku3
+from datetime import datetime
+from config import TX1, TX2, TX3, TX4, TX5 , TX6, TX7, TX8, TX9, TX10, OWNER_ID, SUDO_USERS, HEROKU_APP_NAME, HEROKU_API_KEY, CMD_HNDLR as hl
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon import events
 
+
+@TX1.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@TX2.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@TX3.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@TX4.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@TX5.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@TX6.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@TX7.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@TX8.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@TX9.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@TX10.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+async def ping(e):
+    if e.sender_id in SUDO_USERS:
+        start = datetime.now()
+        toxic = await e.reply(f"» #TOXIC_ᴏᴘ_ʙᴏʟᴛᴀ", parse_mode=None, link_preview=None)
+        end = datetime.now()
+        mp = (end - start).microseconds / 1000
+        await toxic.edit(f"__🤖 ᴘɪɴɢ__\n» `{mp} ms`")
+
+
+@TX1.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
+@TX2.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
+@TX3.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
+@TX4.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
+@TX5.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
+@TX6.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
+@TX7.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
+@TX8.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
+@TX9.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
+@TX10.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
+async def restart(e):
+    if e.sender_id in SUDO_USERS:
+        await e.reply(f"**ᴇʀʀᴏʀ 131**: ꜱᴇʀᴠᴇʀ ɪꜱ ʀᴇꜱᴛᴀʀᴛɪɴɢ 🥵")
+        try:
+            await TX1.disconnect()
+        except Exception:
+            pass
+        try:
+            await TX2.disconnect()
+        except Exception:
+            pass
+        try:
+            await TX3.disconnect()
+        except Exception:
+            pass
+        try:
+            await TX4.disconnect()
+        except Exception:
+            pass
+        try:
+            await TX5.disconnect()
+        except Exception:
+            pass
+        try:
+            await TX6.disconnect()
+        except Exception:
+            pass
+        try:
+            await TX7.disconnect()
+        except Exception:
+            pass
+        try:
+            await TX8.disconnect()
+        except Exception:
+            pass
+        try:
+            await TX9.disconnect()
+        except Exception:
+            pass
+        try:
+            await TX10.disconnect()
+        except Exception:
+            pass
+
+        os.execl(sys.executable, sys.executable, *sys.argv)
+        quit()
+        
+
+Heroku = heroku3.from_key(HEROKU_API_KEY)
+sudousers = os.environ.get("SUDO_USER", None)
+
+@TX1.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
+@TX2.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
+@TX3.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
+@TX4.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
+@TX5.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
+@TX6.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
+@TX7.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
+@TX8.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
+@TX9.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
+@TX10.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
+async def addsudo(event):
+    if event.sender_id == OWNER_ID:
+        ok = await event.reply(f"» __ᴀᴅᴅɪɴɢ ᴜꜱᴇʀ ᴀꜱ ꜱᴜᴅᴏ...__")
+        txs = "SUDO_USER"
+        target = ""
+        if HEROKU_APP_NAME is not None:
+            app = Heroku.app(HEROKU_APP_NAME)
+        else:
+            await ok.edit("`[HEROKU]:" "\nPlease Setup Your` **HEROKU_APP_NAME**")
+            return
+        heroku_var = app.config()
+        if event is None:
+            return
+        try:
+            reply_msg = await event.get_reply_message()
+            target = reply_msg.sender_id
+        except Exception:
+            await ok.edit("» ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜꜱᴇʀ !!")
+        if len(sudousers) > 0:
+            newsudo = f"{sudousers} {target}"
+        else:
+            newsudo = f"{target}"
+        await ok.edit(f"» **ɴᴇᴡ ꜱᴜᴅᴏ ᴜꜱᴇʀ**: `{target}`\n» `ʀᴇsᴛᴀʀᴛɪɴɢ ʙᴏᴛ...`")
+        heroku_var[txs] = newsudo   
+   
+     
+async def get_user(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.forward:
+            replied_user = await event.client(GetFullUserRequest(previous_message.forward.sender_id))
+        else:
+            replied_user = await event.client(GetFullUserRequest(previous_message.sender_id))
+    return replied_user.user.id
